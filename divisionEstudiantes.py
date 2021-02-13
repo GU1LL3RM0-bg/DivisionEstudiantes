b24711aca787b6f10ae68acbfc3e2e305282cf48
guillermo <guillermo>
2021-02-13 13:46:22 -0500

    crear la clase grupos

---
+++ divisionEstudiantes.py
@@ -0,0 +1,22 @@
+
+
+
+#crear una clase grupo: nombre, lista estudiantes, lista temas
+
+
+class Group:
+
+    def __init__(self,name,members,topics):
+
+        self.name = name
+        self.members = members
+        self.topics
+
+        
+#verificacion de la entrada
+
+#crear las listas
+
+#repartir los estudiantes
+
+#repartir los temas
