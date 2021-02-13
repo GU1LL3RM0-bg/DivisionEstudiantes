1d1c52dceaca808a0e85508aabf34699d461a341
guillermo <guillermo>
2021-02-13 13:51:36 -0500

    se creo la verificacion de la entrada

--- divisionEstudiantes.py
+++ divisionEstudiantes.py
@@ -1,4 +1,4 @@
-
+import sys
 
 
 #crear una clase grupo: nombre, lista estudiantes, lista temas
@@ -11,10 +11,17 @@ class Group:
         self.name = name
         self.members = members
         self.topics
-
         
 #verificacion de la entrada
 
+try:
+    students = sys.argv[1]
+    topics = sys.argv[2]
+    groups = sys.argv[3]
+except:
+    print("La cantidad de parametros introducidos por consola es incorrecta")
+    print("Ej: $divisionEstudiantes.py est.txt temas.txt #entero")
+    sys.exit(1)
 #crear las listas
 
 #repartir los estudiantes
